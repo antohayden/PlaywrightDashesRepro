be985d3d8ba19e3bf62bb20d55e20c4ddf540b5e
Anthony Hayden <Anthony.Hayden+Ocuco@ocuco.com>
2025-03-20 12:45:03 +0000

    init

---
+++ LoggerRepo/LoggerRepo/DashesIssue.cs
@@ -0,0 +1,21 @@
+﻿// MarkdownRenderingIssueTest.cs
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PlaywrightMarkdownIssueRepro
+{
+    [TestFixture]
+    public class MarkdownRenderingIssueTest : PageTest
+    {
+        [Test]
+        public async Task Test_Timeout_With_HTML_And_Dashes()
+        {
+            await Page.GotoAsync("https://example.com");
+
+            // Intentionally cause a timeout that generates formatted call log
+            await Page.Locator("//button[text()='Sign In']").ClickAsync(new() { Timeout = 1000 });
+        }
+    }
+}
